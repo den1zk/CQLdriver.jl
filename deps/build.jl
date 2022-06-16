f43e8d6ab0399dd8c86b54b57c371a9abe505039
Deniz Kırlıdoğ <deniz.kirlidog@matriksdata.com>
2022-06-16 12:55:53 +0300

    different file locations for apple m1 and intel

--- deps/build.jl
+++ deps/build.jl
@@ -74,8 +74,10 @@ if Sys.isapple()
         cp(sslloc, ssltransferloc)
     end
     isarm === true && run(`cp /opt/homebrew/lib/libcassandra.dylib /usr/local/lib/libcassandra.dylib`)
+    isarm !== false ? run(`sed -i '' -e 's/CASSLIBNAME/libcassandra.dylib/g' ../src/cqlwrapper.jl`) :    
+    run(`sed -i '' -e 's/CASSLIBNAME/\/opt\/homebrew\/lib\/libcassandra.dylib/g' ../src/cqlwrapper.jl`); 
 
-    command = `sed -i '' -e 's/CASSLIBNAME/libcassandra.dylib/g' ../src/cqlwrapper.jl`
+    #command = `sed -i '' -e 's/CASSLIBNAME/libcassandra.dylib/g' ../src/cqlwrapper.jl`
     sedresult = try run(command) catch e false end
     sedresult == false && error("could not build!")
 end
